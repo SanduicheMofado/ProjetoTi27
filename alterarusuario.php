61c071fd38135a23a5f551ac7eef7a4009bc02b3
SanduicheMofado <thiagocanabarro10@gmail.com>
2023-02-02 11:34:10 -0300

    adição de home e alterarusuário

---
+++ alterarusuario.php
@@ -0,0 +1,44 @@
+<?php
+include("conectadb.php");
+if($_SERVER['REQUEST_METHOD']=='POST'){
+$id=$_POST['id'];
+$nome=$_POST['nome'];
+$senha=$_POST['senha'];
+$sql="UPDATE usuarios SET usu_senha='$senha',usu_nome='$nome' WHERE usu_id='$id'";
+mysqli_query($link,$sql);
+header('location:listausuarios.php');
+echo"<script>window.alert('USUÁRIO ALTERADO COM SUCESSO');</script>";
+exit;
+}
+//Capturar id via GET
+$id=$_GET['id'];
+$sql="SELECT * FROM usuarios WHERE usu_id='$id'";
+$resultado=mysqli_query($link,$sql);
+while($tbl=mysqli_fetch_array($resultado)){
+    $nome=$tbl[1];
+    $senha=$tbl[2];
+}
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ALTERAR USUÁRIO</title>
+    <link rel="stylesheet" href="estilo.css">
+</head>
+<body>
+    <div>
+        <form action="alterarusuario.php" method="post">
+        <input type="hidden" value="<?=$id?>" name="id" required>    
+        <label>NOME</label>
+        <input type="text" name="nome" id="nome" value="<?=$nome?>"required>
+        <label>SENHA</label>
+        <input type="password" name="senha" id="senha" value="<?=$senha?>"required>
+        <br>
+        <input type="submit" value="SALVAR">
+        </form>
+    </div>
+</body>
+</html>
--- listausuarios.php
+++ listausuarios.php
@@ -28,7 +28,7 @@ $resultado = mysqli_query($link, $sql)
                 ?>
                 <tr>
                 <td><?=$tbl[1]?></td>
-                <td><a href="alterarusuario.php?id=<?=$tb[0]?>"><input type="button" value="ALTERAR"></a></td>
+                <td><a href="alterarusuario.php?id=<?=$tbl[0]?>"><input type="button" value="ALTERAR"></a></td>
                 <td><a href="excluirusuario.php?id=<?=$tb[0]?>"><input type="button" value="EXCLUIR"></a></td>
                 </tr>
                 <?php
